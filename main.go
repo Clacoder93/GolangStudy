23c1375e5215779630cd19ea2b564cd565b2dcfa
Lino Veloso <linoc.veloso@gmail.com>
2019-03-29 17:11:56 -0300

    Update main.go

--- main.go
+++ main.go
@@ -5,5 +5,5 @@ import "fmt"
 func main() {
 	fmt.Println("this is just a fucking test")
 
-	fmt.Println("Vamos ver se vai dar certo?")
+	fmt.Println("Editei o código via git para testar o git pull")
 }
