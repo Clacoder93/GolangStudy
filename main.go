1827df325c53d1c41b1a2a11d2398a943fe7c155
Lino Veloso <linoc.veloso@gmail.com>
2019-03-29 17:10:58 -0300

    this is just a message for testing

--- main.go
+++ main.go
@@ -4,4 +4,6 @@ import "fmt"
 
 func main() {
 	fmt.Println("this is just a fucking test")
+
+	fmt.Println("Vamos ver se vai dar certo?")
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
 Isso é apenas um teste
+Isso é um outro teste vamos ver se vai dar certo
